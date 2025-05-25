39369f20bb676462137974d5e5be356f3009d1c7
Prince Muel <68931805+princemuel@users.noreply.github.com>
2025-05-25 11:04:01 +0100

    chore: remove unused files and flders

--- app/src/scripts/types.d.ts
+++
@@ -1,4 +0,0 @@
-type MapKeys<M extends Map<unknown, unknown>> = Prettify<
-  Array<Parameters<M["get"]>[0]>
->;
-type Prettify<T> = { [K in keyof T]: T[K] } & NonNullable<unknown>;
--- pkg/index.d.ts
+++
@@ -1,7 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-/**
-* @param {string} input
-* @returns {string}
-*/
-export function parse(input: string): string;
